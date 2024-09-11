420258fd755dd42608abc2064eb8ae0c32edbc0d
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2024-09-11 10:14:26 +0900

    チュートリアル第二章

---
+++ MS2024/Assets/Script/Character/Player.cs
@@ -0,0 +1,20 @@
+using Fusion;
+
+public class Player : NetworkBehaviour
+{
+    private NetworkCharacterController _cc;
+
+    private void Awake()
+    {
+        _cc = GetComponent<NetworkCharacterController>();
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (GetInput(out NetworkInputData data))
+        {
+            data.direction.Normalize();
+            _cc.Move(5 * data.direction * Runner.DeltaTime);
+        }
+    }
+}
