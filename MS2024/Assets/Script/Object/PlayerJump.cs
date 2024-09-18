6b51aeca38f8c811ba71b6d9f00619b819be4699
リニューアルマン <pokomal2003@gmail.com>
2024-09-18 12:40:32 +0900

    jamp改善

--- MS2024/Assets/Script/Object/PlayerJump.cs
+++ MS2024/Assets/Script/Object/PlayerJump.cs
@@ -4,6 +4,8 @@ using Fusion;
 
 public class PlayerJump : MonoBehaviour
 {
+    InputAction jumpAction=null;
+
     public void OnJump(InputAction.CallbackContext context)
     {
         GetComponent<NetworkCharacterController>().Jump();
@@ -13,12 +15,25 @@ public class PlayerJump : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        var pinput = GetComponent<PlayerInput>();
+        var actionMap = pinput.actions;
+        jumpAction = actionMap["Jump"];
+        if (jumpAction == null)
+        {
+            Debug.Log("ないよじゃんぷ");
 
-    // Update is called once per frame
-    void Update()
+        }
+    }
+        // Update is called once per frame
+        void Update()
     {
-        GetComponent<Rigidbody>().AddForce(new Vector3(0.0f,10.0f,0.0f));
+        if (jumpAction != null && jumpAction.triggered)
+        {
+            // ボタンが「押されたとき」にだけジャンプを実行する
+            if (jumpAction.ReadValue<float>() > 0) // ボタンが押されている時の値は通常 1.0f
+            {
+                GetComponent<NetworkCharacterController>().Jump();
+            }
+        }
     }
 }
