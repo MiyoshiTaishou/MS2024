38fa086244f21c3f0cea2fecf835f1cc46e63de7
リニューアルマン <pokomal2003@gmail.com>
2024-09-18 10:28:55 +0900

    Padで移動した記念

---
+++ MS2024/Assets/Script/Object/PlayerMove.cs
@@ -0,0 +1,22 @@
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log("ƒWƒƒƒ“ƒv‚µ‚Ü‚µ‚½");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
