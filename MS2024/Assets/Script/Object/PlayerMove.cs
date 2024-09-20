8173ba1ec63b7e1e0a8d87c6eb3012799b5cc552
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2024-09-20 16:55:32 +0900

    Revert "移動"
    This reverts commit a522e7bfe328bec1ebf5d112898c08036bde4fc5.

--- MS2024/Assets/Script/Object/PlayerMove.cs
+++ MS2024/Assets/Script/Object/PlayerMove.cs
@@ -1,49 +1,23 @@
+using UnityEngine.InputSystem;
 using UnityEngine;
-using Fusion;
 
-public class PlayerMove : NetworkBehaviour
+public class PlayerMove : MonoBehaviour
 {
-    private Rigidbody rb;
 
-    [SerializeField, Header("加速度")]
-    private float acceleration = 10f; // 加速度
-    [SerializeField, Header("最大速度")]
-    private float maxSpeed = 5f; // 最大速度
-
-    private Vector3 currentVelocity;
-
-    private void Awake()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        rb = GetComponent<Rigidbody>();
-        if (rb == null)
-        {
-            rb = gameObject.AddComponent<Rigidbody>();
-        }
+        Debug.Log("ジャンプしました");
 
-        // Rigidbodyの設定
-        rb.constraints = RigidbodyConstraints.FreezeRotation; // 回転を固定
-        rb.useGravity = false; // 重力を使わない場合
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-       
-    }
-
-    public override void FixedUpdateNetwork()
-    {
-        // ネットワークインプットデータを受け取り計算する
-        if (GetInput(out NetworkInputData data))
-        {
-            // 入力方向のベクトルを正規化する
-            data.direction.Normalize();
-
-            // 加速の計算を行う
-            Vector3 targetVelocity = data.direction * maxSpeed;
-            currentVelocity = Vector3.MoveTowards(currentVelocity, targetVelocity, acceleration * Time.deltaTime);
-
-            // 物理的な移動を行う
-            rb.velocity = currentVelocity;
-        }
+        
     }
 }
