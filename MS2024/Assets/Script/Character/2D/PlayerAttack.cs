8d180b47702c1b7790a03c44e348dfb4c6506e5f
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2024-09-25 12:26:41 +0900

    Revert "Merge branch 'Miyoshi' of https://github.com/MiyoshiTaishou/MS2024 into Miyoshi"
    This reverts commit 899e5621fbcb7ebb52ab007478b0588869990c26, reversing
    changes made to d48c52feb151a27ef7f86922add78673abf88c6e.

--- MS2024/Assets/Script/Character/2D/PlayerAttack.cs
+++ MS2024/Assets/Script/Character/2D/PlayerAttack.cs
@@ -4,56 +4,39 @@ using Fusion;
 
 public class PlayerAttack : NetworkBehaviour
 {
-    //UŒ‚”»’èobj
+    //ƒpƒŠƒB”ÍˆÍ
     [SerializeField, Tooltip("ƒpƒŠƒB‰ÂŽ‹‰»—p")] private GameObject AttackArea;
 
-    //UŒ‚‚Ì”­¶ƒtƒŒ[ƒ€
-    [SerializeField, Tooltip("UŒ‚‚Ì”­¶ƒtƒŒ[ƒ€")] int AttackStartuptime = 50;
+    //ƒpƒŠƒB‚ÌŒø‰ÊŽžŠÔ
+    [SerializeField, Tooltip("UŒ‚‚ÌŽ‘±ƒtƒŒ[ƒ€")] int ParryActivetime = 300;
 
-    //UŒ‚‚ÌŽ‘±ŽžŠÔ
-    [SerializeField, Tooltip("UŒ‚‚ÌŽ‘±ƒtƒŒ[ƒ€")] int AttackActivetime = 300;
-
-    //UŒ‚”ÍˆÍ
-    [SerializeField, Tooltip("UŒ‚”ÍˆÍ")]
-    Vector3 scale= Vector3.one;
-
-    [SerializeField, ReadOnly] int Count = 0;
-    [SerializeField, ReadOnly] bool isAttack=false;
-    [SerializeField, ReadOnly] int AttackCount;
+    int Count = 0;
 
     public void OnAttack(InputAction.CallbackContext context)
     {
-        if (AttackArea.active == false&&context.started)
+        if (context.started)
         {
             Debug.Log("UŒ‚");
-            Count = AttackStartuptime;
-            isAttack = true;
+            AttackArea.SetActive(true);
+            Count = ParryActivetime;
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        AttackArea.transform.localScale = scale;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        AttackArea.transform.localScale = scale;
-
-        if (isAttack && Count>0)
+        if (AttackArea.active == false)
         {
             Count--;
         }
-        else if(AttackArea.active == false && Count <= 0 &&isAttack)
-        {
-            AttackArea.SetActive(true);
-            Count = AttackActivetime;
-        }
-        if(AttackArea.active == true && Count <= 0) 
+        if(Count <= 0) 
         {
             AttackArea.SetActive(false);
-            isAttack = false;
         }
     }
 }
