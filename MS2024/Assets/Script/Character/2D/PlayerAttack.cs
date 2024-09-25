aae1a01b4216b8ac36cd9c56693228b99f093bac
リニューアルマン <pokomal2003@gmail.com>
2024-09-25 10:35:44 +0900

    通常攻撃途中1

--- MS2024/Assets/Script/Character/2D/PlayerAttack.cs
+++ MS2024/Assets/Script/Character/2D/PlayerAttack.cs
@@ -4,6 +4,23 @@ using Fusion;
 
 public class PlayerAttack : NetworkBehaviour
 {
+    //パリィ範囲
+    [SerializeField, Tooltip("パリィ可視化用")] private GameObject AttackArea;
+
+    //パリィの効果時間
+    [SerializeField, Tooltip("攻撃の持続フレーム")] int ParryActivetime = 300;
+
+    int Count = 0;
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("攻撃");
+            AttackArea.SetActive(true);
+            Count = ParryActivetime;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +30,13 @@ public class PlayerAttack : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (AttackArea.active == false)
+        {
+            Count--;
+        }
+        if(Count <= 0) 
+        {
+            AttackArea.SetActive(false);
+        }
     }
 }
