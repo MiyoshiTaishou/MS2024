2509e45ae9fc7084dd42b7f80e2ff5b0aec33b0a
リニューアルマン <pokomal2003@gmail.com>
2024-09-25 09:58:35 +0900

    通常攻撃の作りかけ

---
+++ MS2024/Assets/Script/Character/2D/PlayerAttack.cs
@@ -0,0 +1,18 @@
+using UnityEngine.InputSystem;
+using UnityEngine;
+using Fusion;
+
+public class PlayerAttack : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
