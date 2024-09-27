eea80fe2310b1d085aecc771e62f19dd2cdb5a0e
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2024-09-27 16:34:16 +0900

    プレイヤーのステートシステム作成

---
+++ MS2024/Assets/Script/Character/PlayerState/PlayerMoveState.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveState : IState
+{
+    private PlayerState character;
+
+    private Vector2 moveInput;
+
+    private float currentSpeed;  // 現在の移動速度
+
+
+    public PlayerMoveState(PlayerState character)
+    {
+        this.character = character;
+    }
+
+    public void Enter()
+    {
+        // キャラクターが移動状態に入るときの処理
+        //character.SetAnimation("Idle");
+        Debug.Log("移動処理に入ります");
+    }
+
+    public void Update()
+    {
+        // 入力から移動ベクトルを取得
+        moveInput = character.input.actions["Move"].ReadValue<Vector2>();
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y).normalized;
+
+        // 加速度を加味した速度の更新
+        currentSpeed += character.moveSpeedAcc * Time.deltaTime;
+
+        // 速度を最大速度で制限
+        currentSpeed = Mathf.Min(currentSpeed, character.maxSpeed);
+
+        // 移動を適用
+        character.transform.Translate(move * currentSpeed * Time.deltaTime, Space.World);
+
+        if(moveInput.x == 0.0f && moveInput.y == 0.0f)
+        {
+            character.ChangeState(new PlayerIdleState(character));
+        }
+    }
+
+    public void Exit()
+    {
+        // Idle状態を抜けるときの処理
+    }
+}
---
+++ MS2024/Assets/Script/Character/PlayerState/PlayerState.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// プレイヤーステート管理クラス
+/// </summary>
+public class PlayerState : MonoBehaviour
+{
+    private IState currentState;
+    [HideInInspector]public PlayerInput input;
+
+    // インスペクターで調整可能な移動速度と加速度
+    [HideInInspector] public float moveSpeed = 5.0f;
+    [HideInInspector] public float moveSpeedAcc = 1.0f;
+    [HideInInspector] public float maxSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<PlayerInput>();
+
+        // 初期状態を移動状態にセット (他の状態にする場合は変更)
+        currentState = new PlayerIdleState(this);
+        currentState.Enter();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //現在の状態のUpdate処理を実行
+        currentState.Update();
+    }
+
+    /// <summary>
+    /// 状態の変更
+    /// </summary>
+    /// <param name="newState"></param>
+    public void ChangeState(IState newState)
+    {
+        currentState.Exit();
+        currentState = newState;
+        currentState.Enter();
+    }
+
+    // キャラクターのアニメーションを設定するメソッド
+    public void SetAnimation(string animationName)
+    {
+        // アニメーションのセット処理
+    }
+}
