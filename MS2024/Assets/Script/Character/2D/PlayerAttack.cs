cc4135664b9f79a2f911441297f38e7c5e4eac08
リニューアルマン <pokomal2003@gmail.com>
2024-10-03 19:55:22 +0900

    コンパイルエラー消したりステートマシン無力化したり

--- MS2024/Assets/Script/Character/2D/PlayerAttack.cs
+++ MS2024/Assets/Script/Character/2D/PlayerAttack.cs
@@ -1,104 +1,105 @@
-using UnityEngine.InputSystem;
-using UnityEngine;
+//using UnityEngine.InputSystem;
+//using UnityEngine;
 
-public class PlayerAttack : IState
-{
-    private PlayerState character;
-    public PlayerAttack(PlayerState character)
-    {
-        this.character = character;
-    }
+//public class PlayerAttack : IState
+//{
+//    private PlayerState character;
+//    public PlayerAttack(PlayerState character)
+//    {
+//        this.character = character;
+//    }
 
-    GameObject AttackArea;
+//    GameObject AttackArea;
 
-    //何連撃目
-    static int nHit = 0;
-    //最大連撃数
-    int nMaxHit = 2;
-    public int GetHit() {return nHit;}
-    public void AddHit()
-    {
-        nHit++;
-        if(nHit>nMaxHit)
-        {
-            nHit = 0;
-        }
-        Debug.Log("連撃数:" + nHit);
-    }
-    enum AttackState
-    {
-        None,Startup,Active,Recovery
-    }
+//    //何連撃目
+//    static int nHit = 0;
+//    //最大連撃数
+//    int nMaxHit = 2;
+//    public int GetHit() {return nHit;}
+//    public void AddHit()
+//    {
+//        nHit++;
+//        if(nHit>nMaxHit)
+//        {
+//            nHit = 0;
+//        }
+//        Debug.Log("連撃数:" + nHit);
+//    }
+//    enum AttackState
+//    {
+//        None,Startup,Active,Recovery
+//    }
 
-    AttackState state=AttackState.None;
+//    AttackState state=AttackState.None;
 
-    public void OnAttack(InputAction.CallbackContext context)
-    {
-    }
+//    public void OnAttack(InputAction.CallbackContext context)
+//    {
+//    }
 
-    public void Enter()
-    {
-        AttackArea = character.transform.Find("PlayerAttackArea").gameObject;
-        if (character.isAttack == false)
-        {
-            Debug.Log("攻撃");
-            character.AttackCount = character.AttackStartupFrame;
-            state = AttackState.Startup;
-            character.isAttack = true;
-        }
-        else if (nHit == 2)
-        {
-            Debug.Log("連携攻撃");
-            character.AttackCount = character.AttackStartupFrame;
-            state = AttackState.Startup;
-            character.isAttack = true;
+//    public void Enter()
+//    {
+//        AttackArea = transform.Find("PlayerAttackArea").gameObject;
+//        if (isAttack == false)
+//        {
+//            Debug.Log("攻撃");
+//            AttackCount = AttackStartupFrame;
+//            state = AttackState.Startup;
+//            isAttack = true;
+//        }
+//        else if (nHit == 2)
+//        {
+//            Debug.Log("連携攻撃");
+//            AttackCount = AttackStartupFrame;
+//            state = AttackState.Startup;
+//            isAttack = true;
 
-        }
-    }
-    public void Exit() 
-    {
+//        }
+//    }
+//    public void Exit() 
+//    {
 
-    }
+//    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//    }
 
-    // Update is called once per frame
-    public void Update()
-    {
-        switch(state)
-        {
-            case AttackState.None:
-                break;
-            case AttackState.Startup:
-                character.AttackCount--;
-                if(character.AttackCount <=0) 
-                {
-                    state= AttackState.Active;
-                    AttackArea.SetActive(true);
-                    character.AttackCount = character.AttackActiveFrame;
-                }
-                break;
-            case AttackState.Active:
-                character.AttackCount--;
-                if (character.AttackCount <= 0)
-                {
-                    state = AttackState.Recovery;
-                    AttackArea.SetActive(false);
-                    character.AttackCount = character.AttackRecoveryFrame;
-                }
-                break;
-            case AttackState.Recovery:
-                character.AttackCount--;
-                if (character.AttackCount <= 0)
-                {
-                    state = AttackState.None;
-                    character.isAttack = false;
-                    character.AttackCount = 0;
-                }
-                break;
-        }
-    }
-}
+//    // Update is called once per frame
+//    public void Update()
+//    {
+//        switch(state)
+//        {
+//            case AttackState.None:
+//                break;
+//            case AttackState.Startup:
+//                AttackCount--;
+//                if(AttackCount <=0) 
+//                {
+//                    state= AttackState.Active;
+//                    AttackArea.SetActive(true);
+//                    AttackCount = AttackActiveFrame;
+//                }
+//                break;
+//            case AttackState.Active:
+//                AttackCount--;
+//                if (AttackCount <= 0)
+//                {
+//                    state = AttackState.Recovery;
+//                    AttackArea.SetActive(false);
+//                    AttackCount = AttackRecoveryFrame;
+//                }
+//                break;
+//            case AttackState.Recovery:
+//                AttackCount--;
+//                if (AttackCount <= 0)
+//                {
+//                    state = AttackState.None;
+//                    isAttack = false;
+//                    AttackCount = 0;
+//                }
+//                break;
+//        }
+//    }
+//}
+
--- MS2024/Assets/Script/Character/PlayerState/PlayerJumpState.cs
+++ MS2024/Assets/Script/Character/PlayerState/PlayerJumpState.cs
@@ -1,77 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-/// <summary>
-/// ジャンプ関連の処理
-/// </summary>
-public class PlayerJumpState : IState
-{
-    private PlayerState character;    
-    private Rigidbody rb;
-    private Vector2 moveInput;
+///// <summary>
+///// ジャンプ関連の処理
+///// </summary>
+//public class PlayerJumpState : IState
+//{
+//    private PlayerState character;    
+//    private Rigidbody rb;
+//    private Vector2 moveInput;
 
-    private float rayDistance = 1.5f;     
-    private string groundTag = "Ground";
+//    private float rayDistance = 1.5f;     
+//    private string groundTag = "Ground";
 
-    public PlayerJumpState(PlayerState character)
-    {
-        this.character = character;
-    }
+//    public PlayerJumpState(PlayerState character)
+//    {
+//        this.character = character;
+//    }
 
-    public void Enter()
-    {
-        rb = character.GetComponent<Rigidbody>();
-        rb.velocity = new Vector3(rb.velocity.x, character.jumpForce, rb.velocity.z);
-    }
+//    public void Enter()
+//    {
+//        rb = GetComponent<Rigidbody>();
+//        rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+//    }
 
-    public void Exit()
-    {
+//    public void Exit()
+//    {
         
-    }
+//    }
 
-    public void Update()
-    {
-        // 入力から移動ベクトルを取得
-        moveInput = character.input.actions["Move"].ReadValue<Vector2>();
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y).normalized;
+//    public void Update()
+//    {
+//        // 入力から移動ベクトルを取得
+//        moveInput = input.actions["Move"].ReadValue<Vector2>();
+//        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y).normalized;
 
-        // 加速度を加味した速度の更新
-        character.currentSpeed += character.moveSpeedAcc * Time.deltaTime;
+//        // 加速度を加味した速度の更新
+//        currentSpeed += moveSpeedAcc * Time.deltaTime;
 
-        // 速度を最大速度で制限
-        character.currentSpeed = Mathf.Min(character.currentSpeed, character.maxSpeed);
+//        // 速度を最大速度で制限
+//        currentSpeed = Mathf.Min(currentSpeed, maxSpeed);
 
-        // Rigidbody による移動
-        Vector3 velocity = move * character.currentSpeed;
-        velocity.y = rb.velocity.y;  // Y軸の速度は変更しない
-        rb.velocity = velocity;      // Rigidbody の速度を設定
+//        // Rigidbody による移動
+//        Vector3 velocity = move * currentSpeed;
+//        velocity.y = rb.velocity.y;  // Y軸の速度は変更しない
+//        rb.velocity = velocity;      // Rigidbody の速度を設定
      
 
-        //落下処理
-        if (rb.velocity.y < 0)
-        {
-            rb.velocity += Vector3.up * Physics.gravity.y * (character.fallMultiplier - 1) * Time.deltaTime;
+//        //落下処理
+//        if (rb.velocity.y < 0)
+//        {
+//            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
 
-            RaycastHit hit;
-            if (Physics.Raycast(character.transform.position, Vector3.down, out hit, rayDistance))
-            {
-                if (hit.collider.CompareTag(groundTag))
-                {
-                    AnimatorStateInfo animStateInfo = character.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
-                    if (!animStateInfo.IsName("APlayerLand"))
-                    {
-                        character.SetAnimation("APlayerLand");
-                    }
-                }
-            }
-        }
+//            RaycastHit hit;
+//            if (Physics.Raycast(transform.position, Vector3.down, out hit, rayDistance))
+//            {
+//                if (hit.collider.CompareTag(groundTag))
+//                {
+//                    AnimatorStateInfo animStateInfo = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+//                    if (!animStateInfo.IsName("APlayerLand"))
+//                    {
+//                        SetAnimation("APlayerLand");
+//                    }
+//                }
+//            }
+//        }
 
-        AnimatorStateInfo landAnimStateInfo = character.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
-        if (landAnimStateInfo.IsName("APlayerLand") && landAnimStateInfo.normalizedTime >= 1.0f)
-            if (rb.velocity.y == 0)
-            {
-                character.ChangeState(new PlayerIdleState(character));
-            }
-    }    
-}   
+//        AnimatorStateInfo landAnimStateInfo = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+//        if (landAnimStateInfo.IsName("APlayerLand") && landAnimStateInfo.normalizedTime >= 1.0f)
+//            if (rb.velocity.y == 0)
+//            {
+//                ChangeState(new PlayerIdleState(character));
+//            }
+//    }    
+//}   
