1e740f8fd9a7d74c7cf869d00b397a77679a9f97
リニューアルマン <pokomal2003@gmail.com>
2024-10-04 16:49:20 +0900

    FPS固定

--- MS2024/Assets/Script/UI/ComboGauge.cs
+++ MS2024/Assets/Script/UI/ComboGauge.cs
@@ -6,31 +6,29 @@ public class ComboGauge : MonoBehaviour
     Slider slider;
     LocalCombo combo;
     private float currentGauge;
-    public float maxGauge = 100f;
+    public float maxGauge = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        slider = GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        slider = GetComponent<Slider>();
-        GameObject[] obj = FindObjectsOfType<GameObject>();
-        foreach (GameObject objs in obj)
-        {
-            if (objs.CompareTag("Player"))
-            {
-                combo = objs.GetComponent<LocalCombo>();
-                maxGauge=combo.GetMaxcombotime();
-                break;
-            }
-        }
         if (combo == null)
         {
-            Debug.LogError("‚±‚ñ‚Ú‚È‚¢‚æ");
+            GameObject[] obj = FindObjectsOfType<GameObject>();
+            foreach (GameObject objs in obj)
+            {
+                if (objs.CompareTag("Player"))
+                {
+                    combo = objs.GetComponent<LocalCombo>();
+                    maxGauge = combo.GetMaxcombotime();
+                    break;
+                }
+            }
         }
         currentGauge = combo.Getcombotime();
         slider.value = currentGauge / maxGauge;
