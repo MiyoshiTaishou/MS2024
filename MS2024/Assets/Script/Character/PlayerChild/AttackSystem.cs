2b5c8efb3451a9394981e413f8d2df61f29ee058
リニューアルマン <pokomal2003@gmail.com>
2024-10-04 15:36:33 +0900

    原初のコンボUI

--- MS2024/Assets/Script/Character/PlayerChild/AttackSystem.cs
+++ MS2024/Assets/Script/Character/PlayerChild/AttackSystem.cs
@@ -4,10 +4,11 @@ public class AttackSystem : MonoBehaviour
 {
     [SerializeField, Tooltip("親オブジェクトを入れる")] GameObject player;
     LocalPlayerAttack attack;
-
+    LocalCombo combo;
     // Start is called before the first frame update
     void Start()
     {
+        combo= player.GetComponent<LocalCombo>();
         attack = player.GetComponent<LocalPlayerAttack>();
         if(attack == null)
         {
@@ -26,7 +27,7 @@ public class AttackSystem : MonoBehaviour
         if (other.CompareTag("Enemy"))
         {
             attack.AddHit();
+            combo.AddCombo();
         }
-
     }
 }
