8a260c1a2cd3003eedeb7bca749f964e0fbddaab
mathuki-takumi <124752059+mathuki-takumi@users.noreply.github.com>
2024-10-18 11:18:54 +0900

    途中経過

--- MS2025/Assets/Script/Enemy/Boss_Bard/BossEnemy.cs
+++ MS2025/Assets/Script/Enemy/Boss_Bard/BossEnemy.cs
@@ -5,23 +5,32 @@ using UnityEngine.UI;
 
 public class BossEnemy : MonoBehaviour
 {
+    [Header("HP(float)とHPバー設定")]
     //最大HPと現在のHP。
     public float maxHp = 10;
     float Hp;
     //Slider
     public Slider slider;
 
+    [Space(15)]
+
+    [Header("各エフェクト")]
     //Efect
     [SerializeField]
-    [Tooltip("発生させるエフェクト(パーティクル)")]
-    private ParticleSystem particle;
+    [Tooltip("被ダメージエフェクト")]
+    private ParticleSystem Damageparticle;
 
+    [Space(15)]
+
+    [Header("各サウンドエフェクト")]
     //SoundEfect
     //ならすサウンドエフェクトを入れる変数
-    public AudioClip sound1;
+    [Tooltip("被ダメージSE")]
+    public AudioClip Damagesound;
     AudioSource audioSource;
 
     private Animator m_Animator;
+    private Vector3 location;
 
     void Start()
     {
@@ -120,16 +129,17 @@ public class BossEnemy : MonoBehaviour
             slider.value = Hp;
 
             // パーティクルシステムのインスタンスを生成
-            ParticleSystem newParticle = Instantiate(particle);
+            ParticleSystem newParticle = Instantiate(Damageparticle);
             // パーティクルの発生場所をこのスクリプトをアタッチしているGameObjectの場所にする
-            newParticle.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 4.5f, this.transform.position.z - 1);
+            Vector3 closestPoint = collider.ClosestPoint(location);
+            newParticle.transform.position = closestPoint;
             // パーティクルを発生させる
             newParticle.Play();
             // インスタンス化したパーティクルシステムのGameObjectを1秒後に削除
             Destroy(newParticle.gameObject, 1.0f);
 
             //Sound1を鳴らす
-            audioSource.PlayOneShot(sound1);
+            audioSource.PlayOneShot(Damagesound);
 
             m_Animator.SetTrigger("Hit");
         }
@@ -140,5 +150,6 @@ public class BossEnemy : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.name);
+
     }
 }
