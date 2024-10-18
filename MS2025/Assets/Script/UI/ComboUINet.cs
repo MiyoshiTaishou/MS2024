73b17396fb8289675b7b40013f2c93112dbeca2d
リニューアルマン <pokomal2003@gmail.com>
2024-10-18 17:43:10 +0900

    でけでけsoso

--- MS2025/Assets/Script/UI/ComboUINet.cs
+++ MS2025/Assets/Script/UI/ComboUINet.cs
@@ -1,19 +1,32 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Fusion;
+using TMPro;
 
-public class ComboUINet : MonoBehaviour
+public class ComboUINet : NetworkBehaviour
 {
-    Text text;
+    TextMeshPro text;
     GameObject netobj;
     ShareNumbers combo;
 
     // Start is called before the first frame update
     void Start()
     {
-        text = GetComponent<Text>();
+        text = GetComponent<TextMeshPro>();
         netobj = GameObject.Find("Networkbox");
         if(netobj != null )
         combo = netobj.GetComponent<ShareNumbers>();
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        netobj = GameObject.Find("Networkbox");
+        if (netobj != null)
+        {
+            combo = netobj.GetComponent<ShareNumbers>();
+        }
+        Debug.LogError("‚±‚ñ‚Ú‚¨‚¨‚¨‚¨‚¨" + combo.nCombo);
+        text.text = combo.nCombo.ToString();
     }
 
     // Update is called once per frame
