0e45a29a3a6dadb455c5fc5797531256f37faaf7
リニューアルマン <pokomal2003@gmail.com>
2024-10-26 00:28:14 +0900

    かちあげ途中

--- MS2025/Assets/Script/Player/PlayerRaise.cs
+++ MS2025/Assets/Script/Player/PlayerRaise.cs
@@ -8,7 +8,7 @@ public class PlayerRaise : NetworkBehaviour
     PlayerJumpNet jump;
 
     [Networked] private bool isRaise { get; set; }
-
+    public bool GetisRaise() { return isRaise; }
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +20,20 @@ public class PlayerRaise : NetworkBehaviour
         jump= GetComponent<PlayerJumpNet>();
     }
 
+    public override void FixedUpdateNetwork()
+    {
+        if(jump.GetisGround())
+        {
+            isRaise = false;
+        }
+    }
 
     private void OnTriggerEnter(UnityEngine.Collider other)
     {
         if (jump.GetisJumping() && other.GetComponent<ParryDisplayNet>()&&other.transform.parent!=this)
         {
             GetComponent<NetworkRigidbody3D>().Rigidbody.AddForce(new Vector3(0.0f,jumpPower,0.0f),ForceMode.Impulse);
+            isRaise = true;
             Debug.LogError("‚Æ‚ñ‚Å‚é‚æ‚§‚§‚§");
         }
     }
