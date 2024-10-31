5ab65d6b5f49bef58b50bfb18782b59306a55eb5
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2024-10-31 12:19:43 +0900

    攻撃判定消えないバグ修正

--- MS2025/Assets/Script/Boss/BossAttackArea.cs
+++ MS2025/Assets/Script/Boss/BossAttackArea.cs
@@ -7,15 +7,18 @@ public class BossAttackArea : NetworkBehaviour
 {
     GameObject box;
     GameObject parent;
+    private float deactivateTime = 0.5f; // 攻撃エリアの非表示にするまでの時間
+    private float timer;
 
     public override void Spawned()
     {
         box = GameObject.Find("Networkbox");
         parent = transform.parent.gameObject;
+        timer = deactivateTime;
     }
-  
+
     private void OnTriggerEnter(Collider other)
-    {       
+    {
         if (other.CompareTag("Player"))
         {
             if (other.GetComponent<PlayerParryNet>().ParryCheck())
@@ -31,9 +34,21 @@ public class BossAttackArea : NetworkBehaviour
             Debug.Log("攻撃ヒット");
             box.GetComponent<ShareNumbers>().RPC_Damage();
             other.GetComponent<PlayerHP>().RPC_DamageAnim();
-            //parent.GetComponent<BossAI>().RPC_AnimName();
-
             gameObject.SetActive(false);
-        }        
+        }
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        // タイマーを減らし、一定時間後に非表示にする
+        if (timer > 0)
+        {
+            timer -= Runner.DeltaTime;
+            if (timer <= 0)
+            {
+                gameObject.SetActive(false);
+                timer = deactivateTime;
+            }
+        }
     }
 }
