5c958435ec5d3e6e81b7aefbf4c34754acbfb0c2
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2024-11-11 12:39:28 +0900

    シーンをまたいでも共有される処理

---
+++ MS2025/Assets/Script/Manager/ScoreManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ScoreManager
+{
+    public static int maxCombo = 0;
+    public static int maxMultiAttack = 0;
+    public static float clearTime = 0.0f;
+}
