f3f3b42a79ef28cd145f8b2a8e312947177f5a30
リニューアルマン <pokomal2003@gmail.com>
2024-11-14 11:37:18 +0900

    連携攻撃のダメージ量とヒットストップ時間を調整可能にした

--- MS2025/Assets/Script/Player/AttackAreaDamage.cs
+++ MS2025/Assets/Script/Player/AttackAreaDamage.cs
@@ -10,8 +10,12 @@ public class AttackAreaDamage : NetworkBehaviour
     ComboSystem combo;
     PlayerRaise raise;
     [SerializeField, Tooltip("ヒットストップ時間(f)")] int stopFrame;
+    [SerializeField, Tooltip("連携攻撃ヒットストップ時間(f)")] int buddyStopFrame;
+    [SerializeField, Tooltip("連携攻撃フィニッシュヒットストップ時間(f)")] int buddyFinalStopFrame;
 
     [SerializeField, Header("ダメージ量")] int DamageNum = 100;
+    [SerializeField, Header("連携攻撃ダメージ量")] int buddyDamageNum = 100;
+    [SerializeField, Header("連携攻撃フィニッシュダメージ量")] int buddyFinalDamageNum = 100;
 
     public override void Spawned()
     {
@@ -34,21 +38,25 @@ public class AttackAreaDamage : NetworkBehaviour
             {
                 Debug.Log("のけぞってるなう" + other.GetComponent<BossAI>().GetCurrentAction().actionName);
 
-                if (other.GetComponent<BossAI>().GetCurrentAction().actionName=="Idol"
-                    && other.GetComponent<BossAI>().Nokezori>0)
-                {               
-                    if(other.GetComponent<BossAI>().Nokezori==1)
+                //
+                if (other.GetComponent<BossAI>().Nokezori>0)
+                {
+                    if (other.GetComponent<BossAI>().Nokezori == 1)
                     {
-                        other.GetComponent<BossStatus>().RPC_Damage(350);
+                        other.GetComponent<BossStatus>().RPC_Damage(buddyFinalDamageNum);
+                        player.GetComponent<HitStop>().ApplyHitStop(buddyFinalStopFrame);
+                    }
+                    else
+                    {
+                        other.GetComponent<BossStatus>().RPC_Damage(buddyDamageNum);
+                        player.GetComponent<HitStop>().ApplyHitStop(buddyStopFrame);
                     }
                     other.GetComponent<BossAI>().Nokezori--;
                     other.GetComponent<BossAI>().isInterrupted = true;
-                    other.GetComponent<BossStatus>().RPC_Damage(40);
                     Debug.Log("のけぞってるなう" + other.GetComponent<BossAI>().Nokezori);
+                    RPCCombo();
+                    return;
                 }
-                other.GetComponent<BossStatus>().RPC_Damage(DamageNum);
-                sharenum.AddHitnum();
-                RPCCombo();
                 if(raise.GetisRaise())
                 {
                     Debug.Log("龍墜閃");
@@ -58,6 +66,9 @@ public class AttackAreaDamage : NetworkBehaviour
                         other.GetComponent<BossAI>().isDown = true;
                     }
                 }
+                other.GetComponent<BossStatus>().RPC_Damage(DamageNum);
+                sharenum.AddHitnum();
+                RPCCombo();
                 player.GetComponent<HitStop>().ApplyHitStop(stopFrame);
             }
         }      
