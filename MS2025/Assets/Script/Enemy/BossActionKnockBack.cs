6d068dcc2a975e127c0813213c6cf08c58762a00
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2024-11-14 11:36:39 +0900

    ボスのノックバック実装

--- MS2025/Assets/Script/Enemy/BossActionKnockBack.cs
+++ MS2025/Assets/Script/Enemy/BossActionKnockBack.cs
@@ -2,17 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossActionKnockBack : MonoBehaviour
+[CreateAssetMenu(fileName = "KnockBackAction", menuName = "Boss/Actions/KnockBack")]
+public class BossActionKnockBack : BossActionData
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Header("ノックバックする強さ")]
+    private float KnockBackPower = 1.0f;
+
+    /// <summary>
+    /// プレイヤーからボスへのベクトル
+    /// </summary>
+    private Vector3 PBVec;
+
+    [SerializeField,Header("ノックバック時間（これが隙のある時間になる）")]
+    private float waitDuration;
+
+    private float waitStartTime;
+
+    bool isknock = false;
+
+
+    public override void InitializeAction(GameObject boss, Transform player)
     {
-        
+        PBVec = player.transform.position - boss.transform.position;
+
+        waitStartTime = Time.time;
+        boss.GetComponent<Rigidbody>().useGravity = true;
+
+        PBVec.y = 0;
+
+        isknock = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override bool ExecuteAction(GameObject boss)
     {
-        
+        boss.GetComponent<Rigidbody>().useGravity = true;
+
+        if(!isknock)
+        {
+            boss.GetComponent<Rigidbody>().AddForce(-PBVec * KnockBackPower, ForceMode.Impulse);
+            isknock = true;
+        }
+
+
+        if (Time.time - waitStartTime >= waitDuration)
+        {
+
+            return true; // アクション完了
+        }
+
+        return false; // まだ実行中
     }
 }
