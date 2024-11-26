0b9ed7b09fc3a55c0891b8ead116b27c154766c5
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2024-11-26 11:43:54 +0900

    プレイヤーのHPUIバグ修正

--- MS2025/Assets/Script/Boss/BossAttackArea.cs
+++ MS2025/Assets/Script/Boss/BossAttackArea.cs
@@ -103,6 +103,7 @@ public class BossAttackArea : NetworkBehaviour
             }
 
             Debug.Log("UŒ‚ƒqƒbƒg");
+            box.GetComponent<ShareNumbers>().CurrentHP--;
             box.GetComponent<ShareNumbers>().RPC_Damage();
             other.GetComponent<PlayerHP>().RPC_DamageAnim();
             Render();
