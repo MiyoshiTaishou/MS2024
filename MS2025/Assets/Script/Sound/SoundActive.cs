911d3d479d5975569571199410a01be9bfd3f2a1
aoi0708 <119085678+aoi0708@users.noreply.github.com>
2024-11-26 21:00:00 +0900

    config

--- MS2025/Assets/Script/Sound/SoundActive.cs
+++ MS2025/Assets/Script/Sound/SoundActive.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class SoundActive : MonoBehaviour
 {
 
+
+
     // ボタンが押されたときに呼び出されるメソッド
     public void ShowObject()
     {
 
         this.gameObject.SetActive(!gameObject.activeSelf); // オブジェクトを表示
-
     }
 }
