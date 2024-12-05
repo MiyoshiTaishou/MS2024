503d93994f807fa48f28d53841ad833ebe215c80
リニューアルマン <pokomal2003@gmail.com>
2024-12-05 10:32:42 +0900

    プレイヤーカプセルコライダーにした

--- MS2025/Assets/Script/Player/PlayerPush.cs
+++ MS2025/Assets/Script/Player/PlayerPush.cs
@@ -7,24 +7,49 @@ public class PlayerPush : NetworkBehaviour
     Rigidbody rb;
     GameObject player;
     Vector3 playerPos;
+    GameObject otherPlayer;
+
+    [Networked] public bool onPlayer { get; set;}
+
     public override void Spawned()
     {
-        player = transform.parent.gameObject;
+        player = transform.root.gameObject;
     }
     public override void FixedUpdateNetwork()
     {
         playerPos = player.transform.position;
     }
 
-
-    private void OnTriggerStay(Collider other)
+    public override void Render()
     {
-        if(other.CompareTag("Player"))
+        if (onPlayer)
         {
             Vector3 newpos = playerPos;
-            newpos.x += other.transform.position.x < playerPos.x ? 1.0f : -1.0f;
-            playerPos=newpos;
-            Debug.Log("Œˆ‚Ü‚èŽè‰Ÿ‚µo‚µ");
+            Debug.Log("Œˆ‚Ü‚èŽè‰Ÿ‚µo‚µ" + newpos.x);
+
+            newpos.x += otherPlayer.transform.position.x < playerPos.x ? 1.0f : -1.0f;
+            newpos.x = 3;
+            playerPos = newpos;
+            player.transform.position = newpos;
+            Debug.Log("Œˆ‚Ü‚èŽè‰Ÿ‚µo‚µ" + newpos.x);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")&&other.gameObject!=player)
+        {
+            otherPlayer = other.gameObject;
+            onPlayer= true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && other.gameObject != player)
+        {
+            otherPlayer = null;
+            onPlayer= false;
         }
     }
 }
