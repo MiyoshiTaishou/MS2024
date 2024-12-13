29e007b8bbba9a9fb6864b458b9c5088829c245e
すいか <88550655+Suika225@users.noreply.github.com>
2024-12-13 17:06:34 +0900

    エンドロール作成

---
+++ MS2025/Assets/Script/FadeInScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeInScript : MonoBehaviour
+{
+
+	// フェードインのおおよその秒数
+	[SerializeField]
+	private float fadeInTime;
+	// 背景Image
+	private Image image;
+
+	void Start()
+	{
+		image = transform.Find("Panel").GetComponent<Image>();
+		// コルーチンで使用する待ち時間を計測
+		fadeInTime = 1f * fadeInTime / 10f;
+		StartCoroutine("FadeIn");
+	}
+
+	IEnumerator FadeIn()
+	{
+
+		// Colorのアルファを0.1ずつ下げていく
+		for (var i = 1f; i >= 0; i -= 0.1f)
+		{
+			image.color = new Color(0f, 0f, 0f, i);
+			// 指定秒数待つ
+			yield return new WaitForSeconds(fadeInTime);
+		}
+	}
+}
