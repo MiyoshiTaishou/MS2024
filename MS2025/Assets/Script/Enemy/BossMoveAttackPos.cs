e4a5e64b004ff486ff7acf48587f90f6bcb7c73d
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2024-12-18 19:39:44 +0900

    okokokokok

--- MS2025/Assets/Script/Enemy/BossMoveAttackPos.cs
+++ MS2025/Assets/Script/Enemy/BossMoveAttackPos.cs
@@ -76,7 +76,7 @@ public class BossMoveAttackPos : BossActionData
         attackArea.GetComponent<BoxCollider>().size = attackScale;
         attackArea.SetActive(true);
 
-        attackArea.transform.localScale = new Vector3(2f, 2f, 2f);
+        attackArea.transform.localScale = new Vector3(-2f, 2f, 2f);
         attackArea.transform.rotation = Quaternion.identity;             
         attackArea.transform.rotation = Quaternion.Euler(0, 0, rotPunch);       
 
