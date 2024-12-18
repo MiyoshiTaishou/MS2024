c9a2560fc1ee8730063e8d4a89eec46338bb47e1
mathuki-takumi <124752059+mathuki-takumi@users.noreply.github.com>
2024-12-18 17:23:49 +0900

    a

--- MS2025/Assets/Script/UI/DawnParticle.cs
+++ MS2025/Assets/Script/UI/DawnParticle.cs
@@ -6,15 +6,28 @@ using static UnityEngine.ParticleSystem;
 
 public class DawnParticle : NetworkBehaviour
 {
+    [SerializeField,Header("è¡Ç¶ÇÈÇ‹Ç≈ÇÃÉJÉEÉìÉg 100Ç≈ëÂëÃ1ïb")]
+    private float count;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Render()
     {
-        gameObject.SetActive(false);
+        count++;
+        if (count == 600)
+        {
+            gameObject.SetActive(false);
+            count = 0;
+        }
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+
     }
 }
